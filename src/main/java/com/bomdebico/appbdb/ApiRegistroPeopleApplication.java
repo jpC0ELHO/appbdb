07ab7b2c4b3a0edd64a19944820b75dd7424504f
jpc.human() <110858907+jpC0ELHO@users.noreply.github.com>
2025-01-24 12:10:59 -0300

    first commit

---
+++ src/main/java/com/bomdebico/appbdb/ApiRegistroPeopleApplication.java
@@ -0,0 +1,13 @@
+package com.bomdebico.appbdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRegistroPeopleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRegistroPeopleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bomdebico/appbdb/ApiRegistroPeopleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bomdebico.appbdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRegistroPeopleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
